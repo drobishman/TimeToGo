1a64a998934d75de5004a76eb5ed45548c616867
drobishman <adrian.drob88@gmail.com>
2017-01-12 16:28:48 +0100

    updated a lot of things also new api v2 for current position

--- app/src/main/java/it/curdrome/timetogo/MainActivity.java
+++ app/src/main/java/it/curdrome/timetogo/MainActivity.java
@@ -1,19 +1,40 @@
 package it.curdrome.timetogo;
 
 import android.*;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -33,89 +54,77 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity  implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    public static final String TAG = "MainActivity";
+
+    private static final int REQUEST_POSITION = 0;
+
+    private DrawerLayout mDrawerLayout;
+
+    private ListView mDrawerList;
+
+    private String[] categoryList;
 
     private GoogleMap mMap;
 
     private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
 
-    final LatLng STARTING_POINT = new LatLng(41.8549131, 12.6059798); // Milano
-
     final LatLng END_POINT = new LatLng(41.8426285, 12.5864169); // Torino
+
+    private Marker marker ;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LatLng romeLatLng = new LatLng (41.902783, 12.496366);
+
+    private float zoomLevel = 16;
 
     ProgressDialog pDialog; //da mostrare quando calcola il percorso
     List<LatLng> polyz;
     JSONArray array;
 
-    CameraPosition cameraPosition = new CameraPosition.Builder()
-            .target(STARTING_POINT).zoom(11).bearing(10).tilt(30).build();  //animazione su Milano
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        categoryList = getResources().getStringArray(R.array.categories_name);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, categoryList));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        /*
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        */
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    protected void onResume() {
+        super.onResume();
 
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()){
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
         }
 
-        mMap.setMyLocationEnabled(true);
+        if (mMap == null) {
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
 
-        mMap.addMarker(new MarkerOptions()
-                .position(STARTING_POINT)
-                .title("Mia Località")
-                .icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));  //aggiungiamo i marker
+    public void setUpMap(){
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
-        mMap.addMarker(new MarkerOptions().position(END_POINT).title("Arrivo"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(romeLatLng,11));
 
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
 
@@ -129,16 +138,65 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-
+        Button button = (Button) findViewById(R.id.set_start_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMyLocationButtonClick() {
+            public void onClick(View view) {
+                //remove previous current location Marker
+                if (marker != null){
+                    marker.remove();
+                }
 
-                mMap.getMyLocation();
+                double dLatitude = mLastLocation.getLatitude();
+                double dLongitude = mLastLocation.getLongitude();
+                marker = mMap.addMarker(new MarkerOptions().position(new LatLng(dLatitude, dLongitude))
+                        .title("My Location").icon(BitmapDescriptorFactory
+                                .defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(dLatitude, dLongitude), 12));
 
-                return true;
             }
         });
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        setUpMap();
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this,"onConnected", Toast.LENGTH_SHORT).show();
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //position permission has not been granted
+            requestPositionPermission();
+        }
+        else {
+
+            //position permission has been granted
+            mMap.setMyLocationEnabled(true);
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     private class GetDirection extends AsyncTask<String, String, String> {
@@ -159,9 +217,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             Intent i = getIntent();
 
             String stringUrl = "http://maps.googleapis.com/maps/api/directions/json?origin="
-                    + STARTING_POINT.latitude
+                    + mLastLocation.getLatitude()
                     + ","
-                    + STARTING_POINT.longitude
+                    + mLastLocation.getLongitude()
                     + "&destination="
                     + END_POINT.latitude
                     + ","
@@ -257,4 +315,107 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         return poly;
     }
+
+    private void selectItem(int position) {
+        //fragment = null;
+        CharSequence title = null;
+        switch (position) {
+            case 0:
+                Toast toast1 = Toast.makeText(getApplicationContext(), "categoria1",Toast.LENGTH_SHORT);
+                toast1.show();
+                break;
+
+            case 1:
+                Toast toast2 = Toast.makeText(getApplicationContext(), "categoria2",Toast.LENGTH_SHORT);
+                toast2.show();
+                break;
+
+            case 2:
+                Toast toast3 = Toast.makeText(getApplicationContext(), "categoria3",Toast.LENGTH_SHORT);
+                toast3.show();
+                break;
+
+            case 3:
+                Toast toast4 = Toast.makeText(getApplicationContext(), "categoria4",Toast.LENGTH_SHORT);
+                toast4.show();
+                return;
+        }
+
+        mDrawerList.setItemChecked(position, true);
+        try {
+            getActionBar().setTitle(title);
+        } catch (NullPointerException npex) {
+            Log.d("ERROR", "non è stato possibile egeguire il setTitle() sull'actionBar");
+        }
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /**
+     * Requests the Camera permission.
+     * If the permission has been denied previously, a SnackBar will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    private void requestPositionPermission() {
+        Log.i(TAG, "Position permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(position_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(TAG,
+                    "Displaying camera permission rationale to provide additional context.");
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertBuilder.setCancelable(true);
+            alertBuilder.setTitle("Permission necessary");
+            alertBuilder.setMessage("External storage permission is necessary");
+            alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions((Activity) MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_POSITION);}});
+
+            AlertDialog alert = alertBuilder.create();
+            alert.show();
+        } else {
+
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_POSITION);
+        }
+        // END_INCLUDE(position_permission_request)
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+    }
+
 }
